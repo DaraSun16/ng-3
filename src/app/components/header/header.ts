905ef8a1083dfa75978281328b1728082c546714
Natsuyoshi <natsuyoshi.enflammer@gmail.com>
2025-06-15 19:19:32 +0200

    todos services

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TodosService } from '../../services/todos';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
+  providers: [TodosService]
 })
 export class Header {
-  title = signal('My First Angular app');
+  title = ('My First Angular app');
 }
